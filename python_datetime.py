c5206d87a2b1a6bdfec9160cd8ac04866ce18312
michael8335 <yangf_sky@163.com>
2013-02-27 18:09:32 +0800

    Signed-off-by: michael8335 <yangf_sky@163.com>

---
+++ python_datetime.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import time
+import datetime
+import calendar
+
+def getCurrentTimeZone():
+    return time.timezone/3600
+
+def getUTCTime():
+    return datetime.datetime.utcnow().strftime('%Y-%m-%d %X')
+    
+def calculateTime(originalTime,seconds):
+    return time.strftime('%Y-%m-%d %X', time.localtime(time.mktime(time.strptime(str(originalTime),'%Y-%m-%d %X'))+int(seconds)))
+    
+def getWeekday():
+    return time.strftime('%w')
+def getLastDayofMonth(year,month):
+    return calendar.monthrange(int(year), int(month))[1]
